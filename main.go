4783027483834d274498240e21157daa86ef5727
Darren Tsung <darren@onesignal.com>
2019-01-20 09:56:45 -0800

    Add initial webserver and circleci

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+	http.HandleFunc("/hello", helloHandler)
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
