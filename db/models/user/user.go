0f198aa1c9c21e54cfa68ce9b149067bcba3bbbb
Darren Tsung <darren@onesignal.com>
2019-01-28 09:36:13 -0800

    Error handling inside user model, add validate and move to bcrypt model

--- db/models/user/user.go
+++ db/models/user/user.go
@@ -1,10 +1,14 @@
 package user
 
 import (
+	"database/sql"
+	"errors"
 	"strconv"
 	"time"
 
 	"tweeter/db"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 // User represents a user of the application
@@ -15,6 +19,24 @@ type User struct {
 	Created  time.Time
 	Modified time.Time
 }
+
+// MinimumPasswordLength is the minimum length of a password
+var MinimumPasswordLength = 6
+
+// ErrUserNotFound is the error returned when the given user does not exist
+var ErrUserNotFound = errors.New("could not find user from information given")
+
+// ErrInternalError is the error returned when normal flow failed (db error, timeout, etc)
+var ErrInternalError = errors.New("internal error, retry later")
+
+// ErrMismatchedPassword is the error returned by Validate when email / password do not match
+var ErrMismatchedPassword = errors.New("invalid password given")
+
+// ErrPasswordTooShort is the error returned by Create when the password is too short
+var ErrPasswordTooShort = errors.New("password too short")
+
+// ErrUserEmailAlreadyExists is the error returned by Create when the email already exists
+var ErrUserEmailAlreadyExists = errors.New("user already exists for email")
 
 // ID is a type alias for the user's ID type
 type ID uint64
@@ -32,10 +54,28 @@ func ParseID(idString string) (ID, error) {
 
 // Create creates a user in the database and returns the model
 func Create(email, password string) (user User, err error) {
-	var id ID
-	err = db.DB.QueryRowx("INSERT INTO users (email, password) VALUES ($1, $2) RETURNING user_id", email, password).Scan(&id)
+	if len(password) < MinimumPasswordLength {
+		return User{}, ErrPasswordTooShort
+	}
+
+	var hashedPasswordBytes []byte
+	passwordBytes := []byte(password)
+	hashedPasswordBytes, err = bcrypt.GenerateFromPassword(passwordBytes, bcrypt.DefaultCost)
 	if err != nil {
-		return
+		return User{}, ErrInternalError
+	}
+
+	var id ID
+	err = db.DB.QueryRowx(
+		"INSERT INTO users (email, password) VALUES ($1, $2) RETURNING user_id",
+		email, string(hashedPasswordBytes),
+	).Scan(&id)
+	if err != nil {
+		if err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"` {
+			return User{}, ErrUserEmailAlreadyExists
+		}
+
+		return User{}, ErrInternalError
 	}
 
 	user, err = Get(id)
@@ -45,5 +85,33 @@ func Create(email, password string) (user User, err error) {
 // Get queries and return a user if found, otherwise err
 func Get(userID ID) (user User, err error) {
 	err = db.DB.QueryRowx("SELECT * FROM users WHERE user_id = $1", userID).StructScan(&user)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return user, ErrUserNotFound
+		}
+
+		return user, ErrInternalError
+	}
+
 	return
+}
+
+// Validate returns nil if email / password match, otherwise err
+func Validate(email, password string) (err error) {
+	var hashedPassword string
+	err = db.DB.QueryRowx("SELECT password FROM users WHERE email = $1", email).Scan(&hashedPassword)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return ErrUserNotFound
+		}
+
+		return ErrInternalError
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		return ErrMismatchedPassword
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,6 @@ require (
 	github.com/lib/pq v1.0.0
 	github.com/mattes/migrate v3.0.1+incompatible
 	golang.org/x/arch v0.0.0-20181203225421-5a4828bb7045 // indirect
-	golang.org/x/crypto v0.0.0-20190123085648-057139ce5d2b // indirect
+	golang.org/x/crypto v0.0.0-20190123085648-057139ce5d2b
 	golang.org/x/sys v0.0.0-20190124100055-b90733256f2e // indirect
 )
