dd5dfb4d03a11a4841df25965f6c01d03eca5de1
Darren Tsung <darren@onesignal.com>
2019-03-17 15:27:18 -0700

    Fix comment inside ParseID

--- db/models/user/user.go
+++ db/models/user/user.go
@@ -47,7 +47,7 @@ type ID uint64
 
 // ParseID parses a string into a ID
 func ParseID(idString string) (ID, error) {
-	// Parse the string as base10 into a int64
+	// Parse the string as base10 into a uint64
 	idInt, err := strconv.ParseUint(idString, 10, 64)
 	if err != nil {
 		return 0, ErrUserIDNotValid
