e13c709e80a5a8162efc7119992f449204894f6c
Darren Tsung <darren@onesignal.com>
2019-04-13 14:58:01 -0700

    Refactor render logic into context package
    This avoids a lot of argument pass-through by having the
    entire context available

---
+++ handlers/context/context.go
@@ -0,0 +1,72 @@
+package context
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"tweeter/handlers/responses"
+	"tweeter/metrics"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/sirupsen/logrus"
+)
+
+// Context is a struct with additional utility for the handler func
+type Context struct {
+	endpointName string
+
+	responseWriter http.ResponseWriter
+	request        *http.Request
+}
+
+func New(endpointName string, responseWriter http.ResponseWriter, request *http.Request) Context {
+	return Context{
+		endpointName,
+		responseWriter,
+		request,
+	}
+}
+
+// Logger returns a pre-populated logrus.Entry to log with
+func (c Context) Logger() *logrus.Entry {
+	return logrus.WithFields(logrus.Fields{
+		"endpointName": c.endpointName,
+		"request":      c.request,
+	})
+}
+
+// RenderResponse renders a response from type provided
+func (c Context) RenderResponse(statusCode int, resp interface{}) {
+	respBytes, err := json.Marshal(resp)
+	if err != nil {
+		// This is an error because resp is controlled by the programmer and
+		// should be correct in all situations
+		c.Logger().WithError(err).Error("Resp passed to render was not json.Marshalable")
+		return
+	}
+
+	c.responseWriter.WriteHeader(statusCode)
+	if _, err := c.responseWriter.Write(respBytes); err != nil {
+		c.Logger().WithError(err).Warn("Failed to write bytes to http.ResponseWriter")
+	}
+
+	metrics.APIResponses.
+		With(prometheus.Labels{
+			"endpointName": c.endpointName,
+			"statusCode":   strconv.FormatInt(int64(statusCode), 10),
+		}).Inc()
+}
+
+// RenderErrorResponse renders a standard error with the errors provided
+func (c Context) RenderErrorResponse(statusCode int, errors ...responses.Error) {
+	for _, error := range errors {
+		metrics.APIResponseErrors.
+			With(prometheus.Labels{
+				"endpointName": c.endpointName,
+				"errorTitle":   error.Title,
+			}).Inc()
+	}
+
+	c.RenderResponse(statusCode, responses.NewErrorResponse(errors...))
+}
--- handlers/endpoints/endpoints.go
+++ handlers/endpoints/endpoints.go
@@ -6,12 +6,13 @@ import (
 	"github.com/getsentry/raven-go"
 	"github.com/gorilla/mux"
 
+	"tweeter/handlers/context"
 	"tweeter/handlers/middleware"
 )
 
 // HandlerFunc is type of function used to handle http requests and has
 // additional endpoint.Context
-type HandlerFunc func(http.ResponseWriter, *http.Request, Context)
+type HandlerFunc func(http.ResponseWriter, *http.Request, context.Context)
 
 // Endpoint is the type required to define an endpoint
 type Endpoint struct {
@@ -24,12 +25,7 @@ type Endpoint struct {
 // Attach attaches the endpoint defined to the global http server
 func (e Endpoint) Attach(r *mux.Router) {
 	handler := func(w http.ResponseWriter, r *http.Request) {
-		ctx := Context{
-			endpoint:       &e,
-			responseWriter: w,
-			request:        r,
-		}
-
+		ctx := context.New(e.Name, w, r)
 		e.Handler(w, r, ctx)
 	}
 	handler = middleware.Log(handler)
